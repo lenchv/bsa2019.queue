f148e3cca6088b39b0530afd7d76fad418360502
Volodymyr Lench <lenchvov@rambler.ru>
2019-06-02 14:52:05 +0300

    add docker set up

--- laravel/config/queue.php
+++ laravel/config/queue.php
@@ -43,7 +43,7 @@ return [
 
         'beanstalkd' => [
             'driver' => 'beanstalkd',
-            'host' => 'localhost',
+            'host' => env('BEANSTALK_HOST', 'localhost'),
             'queue' => 'default',
             'retry_after' => 90,
             'block_for' => 0,
